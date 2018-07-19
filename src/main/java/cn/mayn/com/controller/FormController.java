dd434260a460133998ee1be9080806d1a8856233
ZhengDa <1120823170@qq.com>
2018-07-19 23:16:58 +0800

    完成SpringMVC部分

--- src/main/java/cn/mayn/com/controller/FormController.java
+++ src/main/java/cn/mayn/com/controller/FormController.java
@@ -1,4 +1,23 @@
 package cn.mayn.com.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("test")
 public class FormController {
+
+
+    @RequestMapping("get")
+    public String getForm(){
+        return "index.html";
+    }
+
+    @RequestMapping("hello")
+    @ResponseBody
+    public String saveHello(){
+        String hh = "hello world";
+        return hh;
+    }
 }
